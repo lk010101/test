b503b0a95e9577fde17a8a56801d948d0f19be99
lk010101 <kriuchekdesign@gmail.com>
2024-01-07 13:48:22 +0200

    Changes for app.js

--- app.js
+++ app.js
@@ -7,3 +7,5 @@ console.log('Hello, my name is', firstName)
 console.log('My age is', age)
 console.log('I am from', city)
 console.log ('My hobbies are:', hobbies)
+
+console.info("Hello, my name is", firstName, "." , "My age is:" , age, "." , "I am from", city , "." ,  "My hobbies are:" , hobbies , ".")
