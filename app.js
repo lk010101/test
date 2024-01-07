aa59601257fe8f26ca3a5d4b299bf8e5c2278f89
lk010101 <kriuchekdesign@gmail.com>
2024-01-07 13:00:29 +0200

    Changes for app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log ('Hello World')
+let firstName = 'Liza'
+let age = '18+'
+let city = 'Saint Petersburg'
+let hobbies = 'Music and Travel'
+
+console.log('Hello, my name is', firstName)
+console.log('My age is', age)
+console.log('I am from', city)
+console.log ('My hobbies are:', hobbies)
