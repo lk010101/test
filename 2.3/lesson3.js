7ce45e65fc9689e95e6e3f83b712bdadb780dfdb
lk010101 <kriuchekdesign@gmail.com>
2024-01-07 17:19:51 +0200

    2.3

---
+++ 2.3/lesson3.js
@@ -0,0 +1,14 @@
+
+let titleProject = 'Оценка стоимости сайта'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = 600
+let percentage = 15
+let fullPrice = 1000
+let responsive = true
+
+console.log("Project Title:", titleProject)
+console.log("Value of Screens:", screensValue)
+console.log("Price of the Screen:", screenPrice)
+console.log("Percentage:", percentage)
+console.log("Full Price:", fullPrice)
+console.log("Responsive:", responsive)
